af166431325aac3eae5a2142477a982b616d3665
Alexander <Lynx@Alexanders-MacBook-Pro.local>
2016-12-28 20:13:55 +0200

    add data.js

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const projects = [
+    {
+        "age": 0,
+        "id": "yahoo's-data-center",
+        "imageUrl": "img/projects/yahoo's-data-center.jpg",
+        "name": "Yahoo's Data Center",
+        "snippet": "While we’ve had a great deal of experience in building IT related buildings, office and data centers before, this project challenged us to the core.",
+        "description": "This home has been updated and is neat and clean. Open concept floor plan. Large deck perfect for entertaining. Easy to get to. 50 ft of frontage with a concrete seawall to ensure you maintain your property over the years. Large living areas for entertaining year round. If you have wanted to live on the lake, don't let this gem pass you by without taking a look. All measurements are approximate, BATVAI. Bring your offers, and start your summer right. Immediate occupancy so you can move right in. Motivated seller.",
+        "price": "5380",
+        "category": "commercial-projects"
+    },
+    {
+        "age": 1,
+        "id": "erie-lakeside-house-mi",
+        "imageUrl": "img/projects/erie-lakeside-house-mi.jpg",
+        "name": "Erie Lakeside house, MI",
+        "snippet": "Enjoy a refreshing time amidst the monumental Appalachian mountains, in a townhouse that will host the most luxurious holiday for you and your family!",
+        "description": "This home has been updated and is neat and clean. Open concept floor plan. Large deck perfect for entertaining. Easy to get to. 50 ft of frontage with a concrete seawall to ensure you maintain your property over the years. Large living areas for entertaining year round. If you have wanted to live on the lake, don't let this gem pass you by without taking a look. All measurements are approximate, BATVAI. Bring your offers, and start your summer right. Immediate occupancy so you can move right in. Motivated seller.",
+        "price": "4550",
+        "category": "commercial-projects"
+    },
+    {
+        "age": 2,
+        "id": "scottish-blue-and-yellow-dining-hall",
+        "imageUrl": "img/projects/scottish-blue-and-yellow-dining-hall.jpg",
+        "name": "Scottish, blue and yellow dining hall",
+        "snippet": "Enjoy a refreshing time amidst the monumental Appalachian mountains, in a townhouse that will host the most luxurious holiday for you and your family!",
+        "description": "This home has been updated and is neat and clean. Open concept floor plan. Large deck perfect for entertaining. Easy to get to. 50 ft of frontage with a concrete seawall to ensure you maintain your property over the years. Large living areas for entertaining year round. If you have wanted to live on the lake, don't let this gem pass you by without taking a look. All measurements are approximate, BATVAI. Bring your offers, and start your summer right. Immediate occupancy so you can move right in. Motivated seller.",
+        "price": "9500",
+        "category": "commercial-projects"
+    },
+    {
+        "age": 3,
+        "id": "3-storey-apartment-in-cleveland-ohio",
+        "imageUrl": "img/projects/3-storey-apartment-in-cleveland-ohio.jpg",
+        "name": "3-storey apartment in Cleveland, Ohio",
+        "snippet": "Enjoy a refreshing time amidst the monumental Appalachian mountains, in a townhouse that will host the most luxurious holiday for you and your family!",
+        "description": "This home has been updated and is neat and clean. Open concept floor plan. Large deck perfect for entertaining. Easy to get to. 50 ft of frontage with a concrete seawall to ensure you maintain your property over the years. Large living areas for entertaining year round. If you have wanted to live on the lake, don't let this gem pass you by without taking a look. All measurements are approximate, BATVAI. Bring your offers, and start your summer right. Immediate occupancy so you can move right in. Motivated seller.",
+        "price": "9350",
+        "category": "commercial-projects"
+    }
+];
